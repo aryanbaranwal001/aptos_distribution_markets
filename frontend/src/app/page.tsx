beefe67fe10c286a42459931b5a21ae33502c9ef
aryanbaranwal001 <erenyeager108013@gmail.com>
2025-09-30 23:44:33 +0530

    added color extracting

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,27 +9,53 @@ export default function Home() {
   const { color } = useThemeStore();
   const theme = getThemeClasses(color);
 
-  return (
+  const extractHex = (str: string): string | null => {
+    const match = str.match(/#([0-9a-fA-F]{6})/);
+    return match ? `#${match[1]}` : null;
+  };
 
-      <div style={{ width: '100%', height: 1000, position: 'relative' }}>
-        <LiquidEther
-          colors={[ '#5227FF', '#FF9FFC', '#B19EEF' ]}
-          mouseForce={20}
-          cursorSize={100}
-          isViscous={false}
-          viscous={30}
-          iterationsViscous={32}
-          iterationsPoisson={32}
-          resolution={0.4}
-          isBounce={false}
-          autoDemo={true}
-          autoSpeed={0.5}
-          autoIntensity={2.2}
-          takeoverDuration={0.25}
-          autoResumeDelay={3000}
-          autoRampDuration={0.6}
-        />
-      </div>  
+  const uniqueColors: string[] = Array.from(
+    new Set(
+      Object.values(theme)
+        .map(extractHex)
+        .filter((c): c is string => Boolean(c))
+    )
+  );
+
+  const pickByIndexes = (arr: string[], indexes: number[]): string[] => {
+    return indexes
+      .map((i) => arr[i])
+      .filter((c): c is string => Boolean(c)); // remove undefined if index out of bounds
+  };
+
+  const colors: string[] = pickByIndexes(uniqueColors, [0, 1, 2]);
+
+  return (
+<>
+
+      <Navbar />
+      {console.log(colors)}
+
+        <div style={{ width: '100%', height: 1000, position: 'relative' }}>
+          <LiquidEther
+            colors
+            mouseForce={20}
+            cursorSize={100}
+            isViscous={false}
+            viscous={30}
+            iterationsViscous={32}
+            iterationsPoisson={32}
+            resolution={0.4}
+            isBounce={false}
+            autoDemo={true}
+            autoSpeed={0.5}
+            autoIntensity={2.2}
+            takeoverDuration={0.25}
+            autoResumeDelay={3000}
+            autoRampDuration={0.6}
+          />
+        </div>  
+</>
 
   );
 }
