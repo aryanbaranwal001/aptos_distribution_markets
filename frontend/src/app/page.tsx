d8ac60cb2fd6d3fa30dc2e2c165aa0dc44d0d7b6
aryanbaranwal001 <erenyeager108013@gmail.com>
2025-09-30 23:29:13 +0530

    reactbits looking good

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -14,13 +14,13 @@ export default function Home() {
       <div style={{ width: '100%', height: 1000, position: 'relative' }}>
         <LiquidEther
           colors={[ '#5227FF', '#FF9FFC', '#B19EEF' ]}
-          mouseForce={100}
+          mouseForce={20}
           cursorSize={100}
           isViscous={false}
           viscous={30}
           iterationsViscous={32}
           iterationsPoisson={32}
-          resolution={0.5}
+          resolution={0.4}
           isBounce={false}
           autoDemo={true}
           autoSpeed={0.5}
